90874067051469e95c1f8a37c0b3509f0a87d218
Deployment Bot (from Azure Pipelines) <deploy@tokio-rs.com>
2019-08-18 21:54:49 +0000

    Deploy Tokio API documentation

---
+++ doc/aliases.js
@@ -0,0 +1,17 @@
+var ALIASES = {};
+ALIASES["build_tests"] = {};
+ALIASES["cat"] = {};
+ALIASES["exit"] = {};
+ALIASES["tokio"] = {};
+ALIASES["tokio_buf"] = {};
+ALIASES["tokio_codec"] = {};
+ALIASES["tokio_executor"] = {};
+ALIASES["tokio_fs"] = {};
+ALIASES["tokio_io"] = {};
+ALIASES["tokio_macros"] = {};
+ALIASES["tokio_net"] = {};
+ALIASES["tokio_process"] = {};
+ALIASES["tokio_sync"] = {};
+ALIASES["tokio_test"] = {};
+ALIASES["tokio_timer"] = {};
+ALIASES["tokio_tls"] = {};
